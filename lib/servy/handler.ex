1870ab3847ac30e1c2a6c553ee0d834202b4ec57
Arooj <aroojahmad@users.noreply.github.com>
2021-07-10 16:06:47 -0500

    Pattern Matching Maps

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,10 +2,25 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
     |> format_response
   end
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "Warning: #{path} is on the loose!"
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(conv), do: conv
 
   def log(conv), do: IO.inspect conv
 
@@ -17,30 +32,53 @@ defmodule Servy.Handler do
     |> String.split("\n")
     |> List.first
     |> String.split(" ")
-    %{ method: method, path: path, resp_body: "" }
+    %{ method: method,
+    path: path,
+    resp_body: "",
+    status: nil }
   end
 
-  def route(conv) do
-    route(conv, conv.path)
+  #def route(conv) do
+    #route(conv, conv.method, conv.path)
+  #end
+
+  def route(%{method: "GET", path: "/wildthings" } = conv) do
+    %{ conv | status: 200, resp_body: "Bears, Lions, and Tigers" }
   end
 
-  def route(conv, "/wildthings") do
-    %{ conv | resp_body: "Bears, Lions, and Tigers" }
+  def route(%{method: "GET", path: "/bears" } = conv) do
+    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
-  def route(conv, "/bears") do
-    %{conv | resp_body: "Teddy, Smokey, Paddington" }
+  def route(%{method: "GET", path: "/bears/" <> id } = conv) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(%{path: path } = conv) do
+    %{ conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def format_response(conv) do
     # TODO: Use values in the map to create an HTTP response string:
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
     #{conv.resp_body}
     """
   end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+
 
 end
 
@@ -58,7 +96,31 @@ IO.puts response
 
 
 request = """
-GET /bears HTTP/1.1
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
