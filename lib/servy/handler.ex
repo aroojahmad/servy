1da9290e82c104647d4af95b263918ffa5a6faae
Arooj <aroojahmad@users.noreply.github.com>
2021-07-12 18:04:38 -0500

    added file handler

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,6 +1,7 @@
 defmodule Servy.Handler do
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
+  import Servy.FileHandler, only: [handle_file: 2]
   @moduledoc "Handles HTTP requests"
   @pages_path Path.expand("../../pages", __DIR__)
   @doc "Transforms request to a response"
@@ -31,18 +32,6 @@ defmodule Servy.Handler do
       |> Path.join("about.html")
       |> File.read
       |> handle_file(conv)
-  end
-
-  def handle_file({:ok, content}, conv) do
-    %{ conv | status: 200, resp_body: content }
-  end
-
-  def handle_file({:error, :enoent}, conv) do
-    %{ conv | status: 404, resp_body: "File not found!" }
-  end
-
-  def handle_file({:error, reason}, conv) do
-    %{ conv | status: 500, resp_body: "File error: #{reason}" }
   end
 
   # def route(%{method: "GET", path: "/about"} = conv) do
