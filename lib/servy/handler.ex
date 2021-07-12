8f7e475daaaffe74f19e42a1d1855bad4c05bf95
Arooj <aroojahmad@users.noreply.github.com>
2021-07-12 17:59:42 -0500

    Organizing codes

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,4 +1,9 @@
 defmodule Servy.Handler do
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Servy.Parser, only: [parse: 1]
+  @moduledoc "Handles HTTP requests"
+  @pages_path Path.expand("../../pages", __DIR__)
+  @doc "Transforms request to a response"
   def handle(request) do
     request
     |> parse
@@ -9,78 +14,64 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def track(%{status: 404, path: path} = conv) do
-    IO.puts "Warning: #{path} is on the loose!"
-    conv
+  def route(%{ method: "GET", path: "/wildthings" } = conv) do
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
-  def track(conv), do: conv
-
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{ conv | path: "/wildthings" }
+  def route(%{ method: "GET", path: "/bears" } = conv) do
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
-  def rewrite_path(conv), do: conv
-
-  def log(conv), do: IO.inspect conv
-
-  def parse(request) do
-    # TODO: Parse the request string into a map:
-    #elixir map and key value pairs
-    [method, path, _] =
-    request
-    |> String.split("\n")
-    |> List.first
-    |> String.split(" ")
-    %{ method: method,
-    path: path,
-    resp_body: "",
-    status: nil }
-  end
-
-  #def route(conv) do
-    #route(conv, conv.method, conv.path)
-  #end
-
-  def route(%{method: "GET", path: "/wildthings" } = conv) do
-    %{ conv | status: 200, resp_body: "Bears, Lions, and Tigers" }
-  end
-
-  def route(%{method: "GET", path: "/bears" } = conv) do
-    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
-  end
-
-  def route(%{method: "GET", path: "/bears/" <> id } = conv) do
+  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(%{method: "GET", path: "/about" } = conv) do
-    file =
-      Path.expand("../../pages", __DIR__)
+  def route(%{method: "GET", path: "/about"} = conv) do
+      @pages_path
       |> Path.join("about.html")
-
-    case File.read(file) do
-      {:ok, content} ->
-        %{conv | status: 200, resp_body: content}
-
-        {:error, :enoent} ->
-          %{conv | status: 404, resp_body: "File not found" }
-
-        {:error, reason} ->
-          %{ conv | status: 500, resp_body: "File error: #{reason}" }
-    end
+      |> File.read
+      |> handle_file(conv)
   end
 
-  def route(%{path: path } = conv) do
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found!" }
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}" }
+  end
+
+  # def route(%{method: "GET", path: "/about"} = conv) do
+  #   file =
+  #     Path.expand("../../pages", __DIR__)
+  #     |> Path.join("about.html")
+
+  #   case File.read(file) do
+  #     {:ok, content} ->
+  #       %{ conv | status: 200, resp_body: content }
+
+  #     {:error, :enoent} ->
+  #       %{ conv | status: 404, resp_body: "File not found!" }
+
+  #     {:error, reason} ->
+  #       %{ conv | status: 500, resp_body: "File error: #{reason}" }
+  #   end
+  # end
+
+  def route(%{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def format_response(conv) do
-    # TODO: Use values in the map to create an HTTP response string:
     """
     HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
+
     #{conv.resp_body}
     """
   end
@@ -96,7 +87,6 @@ defmodule Servy.Handler do
     }[code]
   end
 
-
 end
 
 request = """
@@ -111,9 +101,8 @@ response = Servy.Handler.handle(request)
 
 IO.puts response
 
-
 request = """
-GET /bears/1 HTTP/1.1
+GET /bears HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
@@ -137,6 +126,18 @@ response = Servy.Handler.handle(request)
 IO.puts response
 
 request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
 GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
@@ -147,6 +148,7 @@ Accept: */*
 response = Servy.Handler.handle(request)
 
 IO.puts response
+
 
 request = """
 GET /about HTTP/1.1
